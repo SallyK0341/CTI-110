1e87c5511c02250d21c33b2b1f5f272ed70b829f
SallyK0341 <150169301+SallyK0341@users.noreply.github.com>
2023-11-07 11:21:51 -0500

    First Upload

---
+++ P1LAB1_SallyKarsten.py
@@ -0,0 +1,9 @@
+#Karsten Sally
+#11/7/23
+#Basic input & output
+
+#Get input from user
+name = input("Please enter your name:")
+
+#Display output to user
+print("Hello", name, "Welcome to CTI-110!")
