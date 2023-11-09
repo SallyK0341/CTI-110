f98af4c7e01761787d8f13b95f2a9b3f0b67237c
SallyK0341 <150169301+SallyK0341@users.noreply.github.com>
2023-11-09 11:08:17 -0500

    Second upload

---
+++ P1HW1.py
@@ -0,0 +1,18 @@
+#Karsten Sally
+#11/7/23
+#Using Integers in math expressions 
+
+#Get input from user
+number1 = int(input("Enter a wholle number:"))
+
+#Display output
+print("You enter: ", number1)
+print(number1, "squared is", number1**2)
+print(number1, "cubed is", number1**3,"!!!")
+
+#Get input from user
+number2 = int(input("Enter another integer:"))
+
+#Display output
+print(number1, "+", number2, "is", number1 + number2)
+print(number1, "*", number2, "is", number1 * number2)
